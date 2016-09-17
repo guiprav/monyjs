3a23d65100d18734b32ae78afad13d6a8b8b3d3b
Guilherme Vieira <super.driver.512@gmail.com>
2016-09-16 23:36:00 -0300

    Implement mony.fromTemplate.

--- mony.js
+++ mony.js
@@ -1,1 +1,11 @@
 const mony = window.mony = {};
+
+mony.fromTemplate = name => {
+  const $el = $(`script[type="template/mony"][name="${name}"]`);
+
+  if (!$el.length) {
+    throw new Error(`No template with name '${name}'`);
+  }
+
+  return $($el.html());
+};
