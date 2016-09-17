e5168cdb146362bce4c77e2992cf60ea82eeb26c
Guilherme Vieira <super.driver.512@gmail.com>
2016-09-17 00:50:18 -0300

    Implement basic routing.

--- mony.js
+++ mony.js
@@ -15,3 +15,93 @@ mony.fromTemplate = name => {
 
   return $($el.html());
 };
+
+mony.routes = [];
+
+mony.routes.notFound = {
+  enter: req => console.error('Route not found:', req),
+};
+
+mony.route = (route, opt) => {
+  mony.routes.push(Object.assign({}, opt, {
+    route,
+  }));
+};
+
+mony.routeNotFound = opt => {
+  mony.routes.notFound = opt;
+};
+
+mony.matchRoute = (route, path) => {
+  const routeNodes = route.split('/');
+  const pathNodes = path.split('/');
+
+  if (routeNodes.length !== pathNodes.length) {
+    return null;
+  }
+
+  const params = {};
+
+  const matches = routeNodes.every((routeNode, i) => {
+    const pathNode = pathNodes[i];
+
+    if (pathNode === undefined) {
+      return false;
+    }
+
+    if (routeNode.startsWith(':')) {
+      params[routeNode.slice(1)] = pathNode;
+      return true;
+    }
+
+    return (routeNode === pathNode);
+  });
+
+  if (!matches) {
+    return null;
+  }
+
+  return params;
+};
+
+$(() => {
+  const $wnd = $(window);
+
+  const last = {};
+
+  $wnd.on('hashchange', ev => {
+    if (last.route && last.route.leave) {
+      last.route.leave(last.req);
+    }
+
+    const path = location.hash.slice(1);
+
+    last.req = {
+      path,
+    };
+
+    const matched = mony.routes.some(reg => {
+      const params = mony.matchRoute(reg.route, path);
+
+      if (!params) {
+        return false;
+      }
+
+      last.req.params = params;
+      last.route = reg;
+
+      if (last.route.enter) {
+        last.route.enter(last.req);
+      }
+
+      return true;
+    });
+
+    if (!matched) {
+      last.route = mony.routes.notFound;
+      last.route.enter(last.req);
+    }
+  });
+
+  $wnd.trigger('hashchange');
+});
